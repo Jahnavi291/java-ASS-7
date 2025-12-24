3cc087287778843fcdd2362900fe936642b88f47
Jahnavi291 <jahnavireddykannapu@gmail.com>
2025-12-24 17:04:20 +0530

    Add files via upload

---
+++ CountEvenOdd.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class CountEvenOdd
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[]a=new int[n];
+        int even=0,odd=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            if(a[i]%2==0)
+            {
+                even++;
+
+            }
+            else{
+                odd++;
+            }
+        }
+        System.out.print(even+"even");
+        System.out.print(odd+"odd");
+    }
+}
